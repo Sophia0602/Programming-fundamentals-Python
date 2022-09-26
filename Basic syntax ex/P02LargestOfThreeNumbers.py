27024d2a6864cc69febcb7fcc5cdf5547514e73a
Sophia <ssooffiiaa16.06@gmail.cpm>
2022-09-26 21:51:50 +0300

    Add taks for exercise
    problem solution fo exercise  for Basic syntax

---
+++ Basic syntax ex/P02LargestOfThreeNumbers.py
@@ -0,0 +1,2 @@
+first_numer, second_number, third_numer = int(input()), int(input()), int(input())
+print(max(first_numer, second_number, third_numer))
---
+++ Basic syntax ex/P05NumberBetween1and100.py ​.py
@@ -0,0 +1,5 @@
+while True:
+    numer = float(input())
+    if 1 <= numer <= 100:
+        print(f"The number {numer} is between 1 and 100")
+        break
